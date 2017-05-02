82196764bfff0fed085da7473dd8d296cc06155b
Friedslick6 <friedslick6@gmail.com>
2017-05-03 02:06:04 +1000

    Script Test

--- JanusVR/Rooms/DOOM/DOOM.js
+++ JanusVR/Rooms/DOOM/DOOM.js
@@ -1,3 +1,7 @@
+var a = jQuery.Event( 'keydown', { which: $.ui.keyCode.F } );
+if (player.flying == 1) {
+    $('input').trigger(a);
+}
 room.onKeyDown = function(event) {
     if (event.keyCode == "F") {
         event.preventDefault();
