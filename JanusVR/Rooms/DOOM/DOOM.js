5473fc96a15d7ecc3cbaa3db5630a8b43cbb49ce
Friedslick6 <friedslick6@gmail.com>
2017-05-03 02:44:42 +1000

    Script Revert

--- JanusVR/Rooms/DOOM/DOOM.js
+++ JanusVR/Rooms/DOOM/DOOM.js
@@ -1,8 +1,8 @@
-room.onLoad() {
-    if (player.flying == 1) {
-        player.flying = 0;
-    }
-}
+//room.onLoad() {
+//    if (player.flying == 1) {
+//        player.flying = 0;
+//    }
+//}
 room.onKeyDown = function(event) {
     if (event.keyCode == "F") {
         event.preventDefault();
